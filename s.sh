240c09958705cce11cf7221b1a46d3a8e7cd20cc
tien <buithithuytien1610@gmail.com>
2024-04-12 23:47:21 +0700

    tien1 vua up codde

---
+++ s.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# silent prompt
+read -p 'GIT profile: ' profile
+
+# switch
+case $profile in
+  tien)
+    git config user.email "buithithuytien1610@gmail.com"
+    git config user.name "tien" 
+    git config user.signingKey "thuytien"
+    ;;
+  tien2)
+    git config user.email "btttien@gmail.com"
+    git config user.name "tien2" 
+    git config user.signingKey "thuytienchan"
+    ;;
+  # default case: raise error
+  *)
+    >&2 echo "ERR: Unknown profile: $profile"
+    exit 1
+esac
