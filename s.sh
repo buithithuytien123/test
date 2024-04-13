8856b327e1a426e61801d8ffeccf3ea435e8a3ea
tien <buithithuytien1610@gmail.com>
2024-04-13 08:10:51 +0700

    add

--- s.sh
+++ s.sh
@@ -8,7 +8,7 @@ case $profile in
   tien)
     git config user.email "buithithuytien1610@gmail.com"
     git config user.name "tien" 
-    git config user.signingKey "thuytien"
+    git config user.signingKey "dPmVjWA25MZdyPwnz8H0UnmPqyRJW1pkpywdUheINhQ"
     ;;
   tien2)
     git config user.email "btttien@gmail.com"
